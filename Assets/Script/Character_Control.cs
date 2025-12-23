a226018575a3d7804eaec931e07a94336e0bed45
BochangZhu <160828488+BochangZhu@users.noreply.github.com>
2025-12-22 21:13:57 -0800

    add main character prefab, attach a test scene

--- Assets/Script/Character_Control.cs
+++ Assets/Script/Character_Control.cs
@@ -2,18 +2,30 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
-    public float jump_strength;
-    private Rigidbody2D myRigidbody;
-    public float mass_scale;
+    public float jump_strength; // Create a jump strength to test for the proper value
+    private Rigidbody2D myRigidbody; // Create RigidBody2D variable
+    public float mass_scale; // Create a mass_scale to control manually
+    private PolygonCollider2D myPolygonCollider; // Create a polygoncollider2d variable
+
+    [SerializeField]
+    private float gravity_scale;
     void Start()
     {
-        myRigidbody = GetComponent<Rigidbody2D>();
-        myRigidbody.mass = mass_scale;
+        Transform myTransform = gameObject.transform;
+        myTransform.position = new Vector3(-6.76f, 0f, 0f);
+
+        myRigidbody = gameObject.AddComponent<Rigidbody2D>(); // Add and store Rigidbody2D
+        myRigidbody.mass = mass_scale; // Adjust the mass value
+        myRigidbody.gravityScale = gravity_scale; //
+
+        myPolygonCollider = gameObject.AddComponent<PolygonCollider2D>(); // Add and store PolygonCollider2D
+
+        name = "squirrel_character"; // Change the name of the character
     }
     
     void Update()
     {
-        if (Input.GetKeyDown("space")){
+        if (Input.GetKeyDown("space")){ // If space is pressed, give the character's an upward velocity
             myRigidbody.linearVelocity = new Vector2(0, 1) * jump_strength;
         }
     }
